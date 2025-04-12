f92367dc0376b62da9d163c6c674aae8df823574
huanillo <102235967+huanillo@users.noreply.github.com>
2025-04-12 17:05:17 +0200

    New improved pseudocode. Operate function redone and finished.

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
-function add(n1, n2){
-return Math.round(n1 + n2);
-}
-function subtract(n1, n2){
-return Math.round(n1 - n2);
-}
-function multiply(n1, n2){
-return Math.round(n1 * n2);
-}
-function divide(n1, n2){
-    if (n2 === 0){
-        return "ERR";
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = null;
+let shouldResetScreen = false;
+
+//WHEN PUSHING A NUMBER:
+    //if currentOperator !== null
+        //add number ti secondOperand
+        //put secondOperand on display
+    //else
+        //if shouldResetScreen == true
+            //reset display
+            //shouldResetScreen = false
+        //else
+            //add num to firstOperand
+            //put firstOperand on display
+
+//WHEN PUSHING AN OPERATOR:
+    //if there is already an operator and secondOperand !== ""
+        //result = operate(currentOperator, firstOperand, secondOperand)
+        //show result ond display
+        //save result as firstOperand
+        //secondOperand = ""
+    //else 
+        //save pushed operator as currentOperator
+        //shouldResetScreen = true
+
+//WHEN PUSHING "=":
+    //if currentOperator and secondOperand are defined:
+        //result = operate(currentOperator, firstOperand, secondOperand)
+        //show result on display
+        //save result as firstOperand
+        //secondOperand = ""
+        //currentOperator = null
+        //shouldResetScreen = true
+
+//WHEN PUSHING CLEAR:
+    //reset firstOperand, secondOperand, currentOperator
+    //reset display
+
+function operate(currentOperator, firstOperand, secondOperand) {
+    n1 = Number(firstOperand);
+    n2 = Number(secondOperand);
+      
+    switch (currentOperator) {
+        case '+':
+            return Math.round(n1 + n2);
+        case '-':
+            return Math.round(n1 - n2);
+        case '*':
+            return Math.round(n1 * n2);
+        case '/':
+            return n2 === 0 ? 'ERR' : Math.round(n1 / n2);
+        default:
+            return null;
     }
-    else {
-        return Math.round(n1 / n2);
-    }
-}
-
-
-function operate(){
-     let n1 = 0;
-     let n2 = 0;
-     let operator = 0;
-     //manejarlo todo con pulsaciones de botones
-
-     //guardar en memoria lo que se vaya pulsando
-     //diferenciar el array de n1, con el operator, con el array de n2
-
-     //un boton de CLEAR que resetee todas las memorias
-     
-
-
-     //cuando se pulsa "=" se llama a la operacion que toque y se genera resultado
 }
